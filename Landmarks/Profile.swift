91477d7a11cb8571b719a227ec7878fac0c1df46
Amit Mahadik <amit.mahadik@stryker.com>
2024-10-28 21:08:45 -0700

    Improved app design and layout.

---
+++ Landmarks/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Mahadik, Amit on 10/28/24.
+//
+
+import Foundation
---
+++ Landmarks/Views/HikeBadge.swift
@@ -0,0 +1,18 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by Mahadik, Amit on 10/28/24.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HikeBadge()
+}
